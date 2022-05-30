91fbb6d17cc462a9252fdb6aa9ba0a53a5b123d6
Sabeli Leyva <106053801+SabeliLeyva@users.noreply.github.com>
2022-05-30 00:15:59 -0500

    Add files via upload
    Factorial

---
+++ Factorial.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+
+{
+//Datos de entrada
+
+int numero;
+	
+for(numero = 1; numero <100; numero += 2)	
+{
+	printf(" %d", numero);
+}
+	
+	return 0;
+}
